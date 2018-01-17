6ac338cf8c845badc787111a8d900965007aed23
Alexey Nikitin <anikitin85@gmail.com>
2018-01-18 00:03:21 +0100

    Basic types added

--- fahrcel1.c
+++ fahrcel1.c
@@ -8,11 +8,20 @@ int main()
     int fahr, celsius;
     int lower, upper, step;
 
+    /*  other basic data types
+        sizes are machine-dependent */
+
+    float flt;      /* floating point number */
+    char chr;       /* character, a single byte */
+    short shrt;     /* short integer */
+    long lng;       /* long integer */
+    double dbl;     /* double-precision fp */
+
     lower = 0;      /* lower limit of temperature table */
     upper = 300;    /* upper limit */
     step = 20;      /* step size */
-
     fahr = lower;
+    
     while (fahr <= upper) {
         celsius = 5 * (fahr - 32) / 9;
         printf("%d\t%d\n", fahr, celsius);
