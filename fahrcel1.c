bab0107250740377acec9a8f813d51128578826c
Alexey Nikitin <anikitin85@gmail.com>
2018-01-18 12:45:16 +0100

    Output formatted

--- fahrcel1.c
+++ fahrcel1.c
@@ -24,7 +24,7 @@ int main()
     
     while (fahr <= upper) {
         celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        printf("%3d %6d\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
