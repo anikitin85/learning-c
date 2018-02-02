65dc4e4b712b17ea458adf365fa42e86047ec980
Alexey Nikitin <anikitin85@gmail.com>
2018-02-02 18:01:35 +0100

    Exercise 2-1: Initial commit

---
+++ includes.c
@@ -0,0 +1,15 @@
+#include <limits.h>
+#include <float.h>
+
+/*  Exercise 2-1: Determine ranges of char, short, int, long
+    (signed and unsigned), then also floats */
+
+    //char - Signed/Unsigned
+    //int, short int, long int - Signed/Unsigned
+    //float
+    //double, long double
+
+int main()
+{
+
+}
