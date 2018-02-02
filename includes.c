a5aaa436f8a58a0f12d143279dd05b29572f260f
Alexey Nikitin <anikitin85@gmail.com>
2018-02-02 21:25:14 +0100

    int, long

--- includes.c
+++ includes.c
@@ -5,10 +5,6 @@
 /*  Exercise 2-1: Determine ranges of char, short, int, long
     (signed and unsigned), then also floats */
 
-    //int, long int - Signed
-    //float
-    //double, long double
-
 int main()
 {
     unsigned char i = 0;
@@ -37,5 +33,38 @@ int main()
     printf("lowest signed char value: %d\n", m);
     printf("highest signed char value: %d\n", j);
     printf("lowest signed char value: %d\n", n);
-    printf("highest signed short int value: %d\n", l);
+    printf("highest signed short int value: %d\n\n", l);
+
+    int a, b;
+    a = 0;
+    b = a + 1;
+    long int c, d;
+    c = 0;
+    d = c + 1;
+    while (a < b) {
+        a++;
+        b++;
+    }
+    while (c < d) {
+        c++;
+        d++;
+    }
+    printf("lowest int value: %ld\n", b);
+    printf("highest int value: %ld\n", a);
+    printf("lowest long int value: %ld\n", d);
+    printf("highest long int value: %ld\n\n", c);
+
+    float e;
+    double f;
+    long double g;
+    e = 0;
+    f = 0;
+    g = 0;
+
+    printf("lowest float value: %f\n", e);
+    printf("highest float value: %f\n", f);
+    printf("lowest double value: %f\n", g);
+    printf("highest double value: %f\n", e);
+    printf("lowest long double value: %f\n", f);
+    printf("highest long double value: %f\n", g);
 }
