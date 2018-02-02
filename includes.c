74e271c93c33e22eda69214e3503b1beeb7d6e64
Alexey Nikitin <anikitin85@gmail.com>
2018-02-02 20:59:49 +0100

    uchar, ushort

--- includes.c
+++ includes.c
@@ -1,15 +1,22 @@
+#include <stdio.h>
 #include <limits.h>
 #include <float.h>
 
 /*  Exercise 2-1: Determine ranges of char, short, int, long
     (signed and unsigned), then also floats */
 
-    //char - Signed/Unsigned
-    //int, short int, long int - Signed/Unsigned
+    //char - Signed
+    //int, short int, long int - Signed
     //float
     //double, long double
 
 int main()
 {
-
+    unsigned char i = 0;
+    unsigned short int k = 0;
+    
+    printf("lowest unsigned char value: %d\n", i);
+    printf("highest unsigned char value: %d\n", --i);
+    printf("lowest unsigned short int value: %d\n", k);
+    printf("highest unsigned short int value: %d\n", --k);
 }
