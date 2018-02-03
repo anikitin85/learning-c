3300151c6e2746e1b0f94b74d80381da97836db9
Alexey Nikitin <anikitin85@gmail.com>
2018-02-03 12:45:58 +0100

    constants

---
+++ constants.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*  Constants */
+int main()
+{
+    // Non-FP are integers:         123     (INT - DEFAULT)
+    // u or U - unsigned:           123U    (UNSIGNED INT)
+    // l or L - long:               123L    (LONG)
+    // ul or UL - unsigned long:    123UL   (UNSIGNED LONG)
+    // FP are double:               123.4   (DOUBLE - DEFAULT)
+    // Numbers with exponent:       123e-4  (DOUBLE)
+    // f or F - float:              123.4F  (FLOAT)
+    // l or L for long double:      123.4L  (LONG DOUBLE)
+
+    // Leading zero - octal:        0123    (OCTAL)
+    // Zero + x - hexadecimal:      0x123   (HEXADECIMAL)
+}
