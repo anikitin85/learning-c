6a9ac62492ad5b4c3e5cc0d36e6457237f176ad2
Alexey Nikitin <anikitin85@gmail.com>
2018-02-05 18:00:54 +0100

    exercise 2-2: initial commit

---
+++ loopwo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*  Exercise 2-2: Write a loop equivalent to this:
+    for (i = 0; i < lim - 1 && (c = getchar()) != '\n' && c != EOF; ++i)
+        s[i] = c;
+    without using && or || */
+
+int main()
+{
+    
+}
