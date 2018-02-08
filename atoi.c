a254dac44f9f9f2e4be1ac2de6f95132733d05fc
Alexey Nikitin <anikitin85@gmail.com>
2018-02-08 12:54:45 +0100

    casting!

--- atoi.c
+++ atoi.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 
 /*  atoi: convert s to int */
 int atoi(char s[]);
@@ -8,7 +9,8 @@ int main()
 {
     char s[] = "123abc";
     printf("%d\n", atoi(s));
-    printf("%c", lower('A'));
+    printf("%c\n", lower('A'));
+    printf("%d", isdigit('3'));
     return 0;
 }
 
@@ -34,3 +36,16 @@ int lower(int c)
 
 // <ctype.h>:   tolower(c) - make lower case
 //              isdigit(c) - test c >= '0' && c <= '9'
+
+// d = c >= '0' && c <= '9'
+// d = 1 if c is a digit
+// d = 0 if c is not a digit
+// but isdigit(c) can return any non-zero value if c is a digit
+// For if/while/for any non-zero is TRUE anyway
+
+// Casting
+// (double) n - convert int n to double
+// Example:
+// sqrt(n) (part of <math.h>) needs double
+// If I want to get a square root of int n,
+// casting is needed: sqrt((double) n)
