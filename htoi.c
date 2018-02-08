8b5e044d988f50f6b2790aa7f6b623657a8dcfaf
Alexey Nikitin <anikitin85@gmail.com>
2018-02-08 17:41:24 +0100

    Intermediate commit

--- htoi.c
+++ htoi.c
@@ -5,14 +5,15 @@
 #define MAXLINE 1000 /* maximum input line length */
 
 int mgetline(char line[], int lim);
-int htoi(char s[]);
+int htoi(char s[], int len);
 
 int main()
 {
+    int len;
     char s[MAXLINE];
-    while (mgetline(s, MAXLINE) > 0)
+    while ((len = mgetline(s, MAXLINE)) > 0)
     {
-        printf("Converted: %x", s, htoi(s));
+        printf("Converted: %x", s, htoi(s, len));
     }
     return 0;
 }
@@ -34,8 +35,16 @@ int mgetline(char s[], int lim)
 }
 
 /* htoi: convert string hex to integer */
-int htoi(char s[])
+int htoi(char s[], int len)
 {
     int res = 0;
+    int prefix = 0;
+    if (len > 2 && s[0] == '0' && s[1] == 'x') {
+        prefix = 1;
+    }
+
+    
+
+
     return res;
 }
