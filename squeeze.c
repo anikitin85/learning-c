dd522553d637e8d29288b48632cb785387d9756a
Alexey Nikitin <anikitin85@gmail.com>
2018-02-09 17:49:25 +0100

    Exercise 2-4: Initial commit

---
+++ squeeze.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*  Exercise 2-4: squeeze(s1, s2)
+    Delete from s1 all chars that appear in s2 */
+
+void squeeze(char s1[], char s2[]);
+
+int main()
+{
+    return 0;
+}
+
+void squeeze(char s1[], char s2[])
+{
+    int i;
+    
+    for (i = 0; s1[i] != '\0'; i++) {
+
+    }
+}
