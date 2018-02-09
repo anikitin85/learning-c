34f284824059ffc83819ca1a7289c89abbe7f509
Alexey Nikitin <anikitin85@gmail.com>
2018-02-09 17:59:45 +0100

    will finish it at home

--- squeeze.c
+++ squeeze.c
@@ -12,9 +12,18 @@ int main()
 
 void squeeze(char s1[], char s2[])
 {
-    int i;
+    int i, j, k;
+    char c;
+    int instr = 0;
     
-    for (i = 0; s1[i] != '\0'; i++) {
-
+    for (i = j = 0; s1[i] != '\0'; i++) {
+        for (k = 0; s2[k] != '\0' && s1[i] != s2[k]; k++) {
+        }
+        if (s2[k] == s1[i])
+            instr = 1;
+        if (!instr) {
+            s1[j++] = s1[i];
+        }
     }
+    s1[j] = '\0';
 }
