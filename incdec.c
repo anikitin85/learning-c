9764af13bb1763c6091d3a6e870bf48c0baca1c5
Alexey Nikitin <anikitin85@gmail.com>
2018-02-09 17:44:16 +0100

    strcat added

--- incdec.c
+++ incdec.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 void squeeze(char s[], int c);
+void strcat(char s[], char t[]);
 
 int main()
 {
@@ -18,4 +19,17 @@ void squeeze(char s[], int c)
         }
     }
     s[j] = '\0';
+}
+
+/*  strcat: concatenate s and t */
+void strcat(char s[], char t[])
+{
+    int i, j;
+
+    i = j = 0;
+    while (s[i] != '\0')    /* find end of s */
+        i++;
+    while ((s[i++] = t[j++]) != '0')    /* copy t */
+        ;
+    
 }
