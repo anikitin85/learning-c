e522e83199ed6535efdd225e94389561674564d2
Alexey Nikitin <anikitin85@gmail.com>
2018-02-12 21:09:01 +0100

    give up again

--- setbits.c
+++ setbits.c
@@ -9,13 +9,17 @@ int main()
     unsigned int input1 = 0;
     unsigned int input2 = 255;
     
-    int output = setbits(input1, 2, 2, input2);
+    int output = setbits(input1, 4, 3, input2);
 
-    printf("Input = %d, %d, result = %d", input1, input2, output);
+    printf("Input1 = %d, input2 = %d\n", input1, input2);
+    printf("Result = %d", output);
     return 0;
 }
 
 int setbits(unsigned int x, int p, int n, unsigned int y)
 {
-    return x | (((y & (0xffff >> n))) << n);
+    // save n lower bits of y in temp 1
+    unsigned int temp1 = (y | (0xffff << n));
+    // save lower n bits of x in temp 2
+    return x;
 }
