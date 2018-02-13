ab1fc4d154c321440809932d9f4c3e0338c8e4e2
Alexey Nikitin <anikitin85@gmail.com>
2018-02-13 21:57:15 +0100

    found solutions to 2-6, 2-7, 2-8

--- setbits.c
+++ setbits.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-int setbits(unsigned int x, int p, int n, unsigned int y);
+unsigned int setbits(unsigned int x, int p, int n, unsigned int y);
+unsigned int invert(unsigned int x, int p, int n);
+unsigned int rightrot(unsigned int x, int n);
 
 /*  Exercise 2-6: setbits(x, p, n, y) */
-
 int main()
 {
     unsigned int input1 = 0;
     unsigned int input2 = 255;
-    
+
     int output = setbits(input1, 4, 3, input2);
 
     printf("Input1 = %d, input2 = %d\n", input1, input2);
@@ -16,11 +17,31 @@ int main()
     return 0;
 }
 
-int setbits(unsigned int x, int p, int n, unsigned int y)
+/*Exercise 2-6. Write a function setbits(x,p,n,y) that returns x with the n bits that begin at position p set to the rightmost n bits of y, leaving the other bits unchanged.*/
+unsigned int setbits(unsigned int x, int p, int n, unsigned int y)
 {
-    // save n lower bits of y in temp 1
-    // I will look for answers online (2-6, 2-7, 2-8, 2-9)
-    unsigned int temp1 = (y | (0xffff << n));
-    // save lower n bits of x in temp 2
+    // get n right-most bits of y only
+    y = ~(~0 << n) & y;
+    // shift those n bits p-n bits to the left
+    y = y << (p - n);
+    // zero out n bits of x starting at p
+    x = x & ~((~(~0 << n)) << (p - n));
+    return x | y;
+}
+
+/*Exercise 2-7. Write a function invert(x,p,n) that returns x with the n bits that begin at position p inverted (i.e., 1 changed into 0 and vice versa), leaving the others unchanged.*/
+unsigned int invert(unsigned int x, int p, int n)
+{
+    return setbits(x, p, n, ~x >> (p - n));
+}
+
+/*Exercise 2-8. Write a function rightrot(x,n) that returns the value of the integer x rotated to the right by n positions.*/
+unsigned int rightrot(unsigned int x, int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        x = setbits(x >> 1, 16, 1, x); // assumes 16-bit
+    }
     return x;
 }
